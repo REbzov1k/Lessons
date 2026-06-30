748ece8fd7ac6ce57315cfd274d7017a77bfbbba
REbzov1k <vahnovan.sun@gmail.com>
2026-06-30 20:10:32 +0300

    corrected text

--- lesson6.py
+++ lesson6.py
@@ -1,5 +1,5 @@
 print("1.AUDI_RS7\n2.BMW_M8\n3.MERCEDES_G-class\n4.PORSCHE_911_gt3_RS")
-number_of_auto = int(input("Введіть ноер машини щоб дізнатись скільки вона коштує:"))
+number_of_auto = int(input("Введіть номер машини щоб дізнатись скільки вона коштує:"))
 if number_of_auto == 1:
     print("150.000-usd")
 elif number_of_auto == 2:
